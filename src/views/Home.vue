e4316dac21c1ba2d3930d1d83b2d1025eee6de9e
Justin Mattson <justin.mattson@outlook.com>
2020-09-12 19:39:06 -0600

    added mttreasures

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -311,6 +311,14 @@ export default {
       ],
       applications: [
         {
+          githubUrl: "https://github.com/JustinMattson/mttreasures",
+          hostName: "Heroku",
+          hostUrl: "https://mttreasures.herokuapp.com/",
+          img: "mttreasures.png",
+          name: "MT Treasure",
+          stack: "Vue.js | Node.js | MongoDB",
+        },
+        {
           githubUrl: "https://github.com/JustinMattson/CS-final-keepr",
           hostName: "Heroku",
           hostUrl: "https://keepr252.herokuapp.com/#/",
